0bba7c2e64ae13f1226275d63f9b82b127adb225
tjdql <tjdqlscjswp@naver.com>
2024-11-18 21:55:13 +0900

    2024-11-18 빌드 에러 수정 테스트

--- src/main/java/com/kosta/toyProject/dto/ItemDto.java
+++ src/main/java/com/kosta/toyProject/dto/ItemDto.java
@@ -2,13 +2,17 @@ package com.kosta.toyProject.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ItemDto {
 
     private Long id;
     private String name;
     private Long count;
+
+
 
     @Builder
     public ItemDto(Long id, String name, Long count) {
